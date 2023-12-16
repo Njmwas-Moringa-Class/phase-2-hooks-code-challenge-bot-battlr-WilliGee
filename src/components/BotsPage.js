83c15fc2ced4434a3d67d9d279a06b3f93c4e6a9
WilliGee <144535344+WilliGee@users.noreply.github.com>
2023-12-16 14:54:22 +0300

    Update BotsPage.js

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
+
 function BotsPage() {
-  //start here with your code for step one
+  const [bots, setBots] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then(response => response.json())
+      .then(setBots)
+  }, [])
+
+  function enlistBot(bot) {
+    console.log(bot);
+    setBots(bots.map(b => b.id === bot.id ? { ...b, army: true } : b));
+  }
+
+  function removeBot(bot) {
+    console.log(bot);
+    setBots(bots.map(b => b.id === bot.id ? { ...b, army: false } : b));
+  }
+
+  function deleteBot(bot) {
+    setBots(bots.filter(b => b.id !== bot.id))
+  }
 
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy bots={bots.filter(b => b.army)} removeBot={removeBot} deleteBot={deleteBot} />
+      <BotCollection bots={bots} enlistBot={enlistBot} deleteBot={deleteBot} />
     </div>
   )
 }
+
 
 export default BotsPage;
